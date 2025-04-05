a260d276a9442755e8fad616a4e4f66f1364f664
aleksanderburlin <f.h.elventor@yandex.ru>
2025-04-05 22:58:39 +0300

    main.go add handlerFunc for test 'NextDate' algorithm

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/joho/godotenv"
 
 	"github.com/Ekvo/yandex-practicum-go-final-project/internal/database"
 	"github.com/Ekvo/yandex-practicum-go-final-project/internal/server"
+	"github.com/Ekvo/yandex-practicum-go-final-project/internal/services"
 )
 
 func init() {
@@ -28,6 +30,30 @@ func main() {
 	srv := server.InitSRV(mux)
 
 	mux.Handle("/", http.FileServer(http.Dir("./web")))
+
+	mux.HandleFunc("/api/nextdate", func(w http.ResponseWriter, r *http.Request) {
+		now := r.URL.Query().Get("now")
+		dstart := r.URL.Query().Get("date")
+		repeat := r.URL.Query().Get("repeat")
+		t := time.Time{}
+		if now == "" {
+			t = time.Now()
+		} else {
+			m, err := time.Parse("20060102", now)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			t = m
+		}
+		newt, err := services.NextDate(t, dstart, repeat)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte(newt))
+	})
 
 	srv.ListenAndServeAndShut(server.ServerTimeoutShut)
 }

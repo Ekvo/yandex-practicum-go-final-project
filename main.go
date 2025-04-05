a328bbe2618554922050d985348664ac420b95b4
aleksanderburlin <f.h.elventor@yandex.ru>
2025-04-05 18:25:02 +0300

    .env (new - TODO_DBFILE), main.go db connect, test: app,db - ok

--- main.go
+++ main.go
@@ -6,15 +6,25 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
+
+	"github.com/Ekvo/yandex-practicum-go-final-project/internal/database"
 )
 
 func init() {
 	if err := godotenv.Load("./init/.env"); err != nil {
-		log.Printf("no .env file - %v", err)
+		log.Printf("main: no .env file error - %v", err)
 	}
 }
 
 func main() {
+	db := database.InitDB()
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Printf("main: sql.DB.Close error - %v", err)
+		}
+	}()
+	_ = database.NewSource(db)
+
 	http.Handle("/", http.FileServer(http.Dir("./web")))
 	if err := http.ListenAndServe(":"+os.Getenv("TODO_PORT"), nil); err != nil {
 		log.Fatalf("stop error - %v", err)

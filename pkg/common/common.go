cdcca8905c6f452a2c9886cd5b484e8a2a47dff5
aleksanderburlin <f.h.elventor@yandex.ru>
2025-04-05 18:23:26 +0300

    New dir: pkg/common: common.go -> general utilities,              CreatePathWithFile(partOfFilePath string) error

---
+++ pkg/common/common.go
@@ -0,0 +1,31 @@
+// common - generate utiles
+package common
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// CreatePathWithFile - create a file and all directories to it if they do not exist
+func CreatePathWithFile(partOfFilePath string) error {
+	fileName := filepath.Base(partOfFilePath)
+	if fileName == "" {
+		return errors.New("incorrect path of file")
+	}
+	if fileExtension := filepath.Ext(fileName); fileExtension != ".db" {
+		return errors.New("invalid file extension")
+	}
+	currentDir, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	fullPath := filepath.Join(currentDir, partOfFilePath)
+	onlyDir := strings.Replace(fullPath, fileName, "", 1)
+	if err := os.MkdirAll(onlyDir, 0o755); err != nil {
+		return err
+	}
+	_, err = os.Create(fullPath)
+	return err
+}

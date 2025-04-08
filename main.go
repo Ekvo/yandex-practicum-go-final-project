a8f670860198cdc82a0a15a5e738931a85e0e36c
aleksanderburlin <f.h.elventor@yandex.ru>
2025-04-08 11:52:55 +0300

    main.go - code compression

--- main.go
+++ main.go
@@ -3,13 +3,11 @@ package main
 import (
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/joho/godotenv"
 
 	"github.com/Ekvo/yandex-practicum-go-final-project/internal/database"
-	"github.com/Ekvo/yandex-practicum-go-final-project/internal/server"
-	"github.com/Ekvo/yandex-practicum-go-final-project/internal/services"
+	"github.com/Ekvo/yandex-practicum-go-final-project/internal/transport"
 )
 
 func init() {
@@ -19,41 +17,17 @@ func init() {
 }
 
 func main() {
-	db := database.InitDB()
+	db, err := database.InitDB()
+	if err != nil {
+		log.Fatalf("main: error - %v", err)
+	}
 	defer func() {
 		if err := db.Close(); err != nil {
 			log.Printf("main: sql.DB.Close error - %v", err)
 		}
 	}()
-	_ = database.NewSource(db)
-	mux := http.NewServeMux()
-	srv := server.InitSRV(mux)
-
-	mux.Handle("/", http.FileServer(http.Dir("./web")))
-
-	mux.HandleFunc("/api/nextdate", func(w http.ResponseWriter, r *http.Request) {
-		now := r.URL.Query().Get("now")
-		dstart := r.URL.Query().Get("date")
-		repeat := r.URL.Query().Get("repeat")
-		t := time.Time{}
-		if now == "" {
-			t = time.Now()
-		} else {
-			m, err := time.Parse("20060102", now)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusBadRequest)
-				return
-			}
-			t = m
-		}
-		newt, err := services.NextDate(t, dstart, repeat)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write([]byte(newt))
-	})
-
-	srv.ListenAndServeAndShut(server.ServerTimeoutShut)
+	r := transport.NewTransport(http.NewServeMux())
+	if err := r.Run(database.NewSource(db)); err != nil {
+		log.Printf("main: error - %v", err)
+	}
 }

c9d926276ca4b6d2fd34a167218c172a5bcac389
aleksanderburlin <f.h.elventor@yandex.ru>
2025-04-08 11:54:23 +0300

    change tests/settings.go

--- tests/settings.go
+++ tests/settings.go
@@ -1,7 +1,9 @@
 package tests
 
-var Port = 7540
-var DBFile = "../scheduler.db"
-var FullNextDate = false
-var Search = false
-var Token = ``
+var (
+	Port         = 8000
+	DBFile       = "../storage/scheduler.db"
+	FullNextDate = true
+	Search       = true
+	Token        = ``
+)

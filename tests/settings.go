7901b46494b55fde7e5c29cbc934876401560bfb
aleksanderburlin <f.h.elventor@yandex.ru>
2025-04-10 10:45:57 +0300

    settings.go: all true, add Token -> test ./... is ok

--- tests/settings.go
+++ tests/settings.go
@@ -5,5 +5,5 @@ var (
 	DBFile       = "../storage/scheduler.db"
 	FullNextDate = true
 	Search       = true
-	Token        = ``
+	Token        = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb250ZW50IjoiVGFzayBBY2Nlc3MiLCJleHBsb3JhdGlvbiI6MTc0NDgyODA1N30.B0kttE1VCbiuFcld3T-MKjnC_AZXVHoCYP-NJ0UtZN8`
 )

08f17f194b92fce61fffe7f02f4a1374fdf6f4e5
aleksanderburlin <f.h.elventor@yandex.ru>
2025-04-10 10:43:25 +0300

    go mod -> add github.com/golang-jwt/jwt/v5 v5.2.2, main.go ->func Init: common.SecretKey = os.Getenv(TODO_SECRET_KEY)

--- main.go
+++ main.go
@@ -3,16 +3,22 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/joho/godotenv"
 
 	"github.com/Ekvo/yandex-practicum-go-final-project/internal/database"
 	"github.com/Ekvo/yandex-practicum-go-final-project/internal/transport"
+	"github.com/Ekvo/yandex-practicum-go-final-project/pkg/common"
 )
 
 func init() {
 	if err := godotenv.Load("./init/.env"); err != nil {
 		log.Printf("main: no .env file error - %v", err)
+	}
+	common.SecretKey = os.Getenv("TODO_SECRET_KEY")
+	if common.SecretKey == "" {
+		log.Print("main: SecretKey is empty")
 	}
 }
 

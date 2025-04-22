91b3295f2cc2795994c8837cd3a9ab628f981394
aleksanderburlin <f.h.elventor@yandex.ru>
2025-04-22 23:30:54 +0300

    cmd/app/main.go: delete func init(), now ccreate config and call Run from internal/app/run.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,38 +2,15 @@ package main
 
 import (
 	"log"
-	"net/http"
-	"os"
 
-	"github.com/joho/godotenv"
-
-	"github.com/Ekvo/yandex-practicum-go-final-project/internal/database"
-	"github.com/Ekvo/yandex-practicum-go-final-project/internal/transport"
-	"github.com/Ekvo/yandex-practicum-go-final-project/pkg/common"
+	"github.com/Ekvo/yandex-practicum-go-final-project/internal/app"
+	"github.com/Ekvo/yandex-practicum-go-final-project/internal/config"
 )
 
-func init() {
-	if err := godotenv.Load("./init/.env"); err != nil {
-		log.Printf("main: no .env file error - %v", err)
-	}
-	common.SecretKey = os.Getenv("TODO_SECRET_KEY")
-	if common.SecretKey == "" {
-		log.Print("main: SecretKey is empty")
-	}
-}
-
 func main() {
-	db, err := database.InitDB(false)
+	cfg, err := config.NewConfig("./init/.env")
 	if err != nil {
 		log.Fatalf("main: error - %v", err)
 	}
-	defer func() {
-		if err := db.Close(); err != nil {
-			log.Printf("main: sql.DB.Close error - %v", err)
-		}
-	}()
-	r := transport.NewTransport(http.NewServeMux())
-	if err := r.Run(database.NewSource(db)); err != nil {
-		log.Printf("main: error - %v", err)
-	}
+	app.Run(cfg)
 }

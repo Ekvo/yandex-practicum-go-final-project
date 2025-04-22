161dcc3bd070dc6beb3da256a721d26b8699e92d
aleksanderburlin <f.h.elventor@yandex.ru>
2025-04-22 23:14:57 +0300

    New file: transport/midlweare.go - describes midlweare function for routes (add func AuthZ and interface rulesForAuthZ)

---
+++ internal/transport/midlweare.go
@@ -0,0 +1,58 @@
+// midlweare - describes the midlweare function
+package transport
+
+import (
+	"context"
+	"errors"
+	"net/http"
+
+	"github.com/Ekvo/yandex-practicum-go-final-project/internal/services"
+
+	"github.com/Ekvo/yandex-practicum-go-final-project/pkg/common"
+)
+
+// rulesForAuthZ - set of rules for 'AuthZ(authCase rulesForAuthZ, next http.HandlerFunc) http.HandlerFunc'
+type rulesForAuthZ interface {
+	services.AutorizationCase
+
+	services.LoginValidPasswordCase
+}
+
+// AuthZ (Autorization)
+//  1. check password (services.LoginValidPassword)
+//
+// password exist  -> (services.Autorization)
+// password !exist -> call  next(w,r)
+//
+//  2. password exist:
+//     use bizness logic of Autorization look (internal/services/usecase/authcase.go)
+//     if error clean all cookie (MaxAge = -1) and see status code
+//     no error -> call next(w,r)
+func AuthZ(authCase rulesForAuthZ, next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ok, err := authCase.UserExist(context.Background())
+		if err != nil {
+			err = services.ErrServicesInternalError
+			common.EncodeJSON(w, http.StatusInternalServerError, common.Message{"error": err.Error()})
+			return
+		}
+		if !ok {
+			next(w, r)
+			return
+		}
+
+		if err := authCase.AuthZ(r); err != nil {
+			code := 0
+			if errors.Is(err, services.ErrServicesInternalError) {
+				code = http.StatusInternalServerError
+			} else {
+				//jwt.ErrTokenExpired
+				code = http.StatusUnauthorized
+			}
+			common.CleanCookie(w, r)
+			common.EncodeJSON(w, code, common.Message{"error": err.Error()})
+			return
+		}
+		next(w, r)
+	}
+}
